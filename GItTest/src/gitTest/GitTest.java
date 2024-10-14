624b3beb8ab880169f543978d5aff8e7acce6810
nadia102323 <nadia102323@gmail.com>
2024-10-14 20:07:07 +0800

    mmmm

--- GItTest/src/gitTest/GitTest.java
+++ GItTest/src/gitTest/GitTest.java
@@ -4,7 +4,8 @@ public class GitTest {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("bruhhhhhhhhh");
+		System.out.println("bruhhhhhhhhh");		System.out.println("Hello World");
+
 
 	}
 }
